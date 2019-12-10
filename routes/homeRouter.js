d9f023683bc9f43646fafd3ad394141a39131375
mkovachev <mkovachev6@gmail.com>
2019-12-10 22:17:46 +0200

    .

--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-const mongoose = require('mongoose');
 
 const User = require('../models/user');
 const Vehicle = require('../models/vehicle');
