762cc906afecfd6ac44e41a153aaf742d83f56e9
mkovachev <mkovachev6@gmail.com>
2019-12-21 10:42:59 +0200

    .

--- routes/eventRouter.js
+++ routes/eventRouter.js
@@ -1,45 +1,47 @@
-const express = require('express');
-const eventRouter = express.Router();
-const loginValidator = require('../middleware/loginValidator');
-const Event = require('../models/event');
+const express = require('express')
+const eventRouter = express.Router()
+const loginValidator = require('../middleware/loginValidator')
+const Event = require('../models/event')
 
-eventRouter.get('/', loginValidator.isLoggedIn, (req, res) => res.render('addevent', {
+eventRouter.get('/', loginValidator.isLoggedIn, (req, res) =>
+  res.render('addevent', {
     layout: false
-}));
+  })
+)
 
 // add ivent
 eventRouter.post('/', loginValidator.isLoggedIn, (req, res) => {
-    const title = req.body.title;
-    const description = req.body.description;
-    const license = req.body.license;
-    const cost = req.body.cost;
+  const title = req.body.title
+  const description = req.body.description
+  const license = req.body.license
+  const cost = req.body.cost
 
-    const user = req.session.user;
+  const user = req.session.user
 
-    // input validation
-    req.checkBody('title', 'title is required').notEmpty();
-    req.checkBody('description', 'description is required').notEmpty();
-    req.checkBody('license', 'license is required').notEmpty();
-    req.checkBody('cost', 'cost is required').notEmpty();
+  // input validation
+  req.checkBody('title', 'title is required').notEmpty()
+  req.checkBody('description', 'description is required').notEmpty()
+  req.checkBody('license', 'license is required').notEmpty()
+  req.checkBody('cost', 'cost is required').notEmpty()
 
-    const errors = req.validationErrors();
-    if (errors) {
-        res.render('addevent', {
-            errors: errors
-        });
-    } else {
-        const newEvent = new Event({
-            title,
-            description,
-            license,
-            cost,
-            'owner': user._id
-        });
-        Event.addEvent(newEvent);
-        req.flash('success_msg', 'Event successfully added!');
-        //vehicle.events.push(newEvent._id); // TODO
-        res.redirect('/maintenance');
-    }
-});
+  const errors = req.validationErrors()
+  if (errors) {
+    res.render('addevent', {
+      errors: errors
+    })
+  } else {
+    const newEvent = new Event({
+      title,
+      description,
+      license,
+      cost,
+      owner: user._id
+    })
+    Event.addEvent(newEvent)
+    req.flash('success_msg', 'Event successfully added!')
+    //vehicle.events.push(newEvent._id); // TODO
+    res.redirect('/maintenance')
+  }
+})
 
-module.exports = eventRouter;
+module.exports = eventRouter
--- server.js
+++ server.js
@@ -1,17 +1,15 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const expressValidator = require('express-validator');
-const exphbs = require('express-handlebars');
-const session = require('express-session');
-const flash = require('connect-flash');
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const expressValidator = require('express-validator')
+const exphbs = require('express-handlebars')
+const session = require('express-session')
+const flash = require('connect-flash')
 
 // set mongoDB
-const mongo = require('mongodb');
-const mongoose = require('mongoose');
-mongoose.set('useCreateIndex', true);
-mongoose.Promise = global.Promise;
-const db = mongoose.connection;
+const mongoose = require('mongoose')
+mongoose.set('useCreateIndex', true)
+mongoose.Promise = global.Promise
 mongoose.createConnection(
   'mongodb://localhost/mygarage',
   {
@@ -20,36 +18,36 @@ mongoose.createConnection(
     useCreateIndex: true
   },
   err => {
-    if (err) console.error(err);
-    else console.log('Connected to the mongodb');
+    if (err) console.error(err)
+    else console.log('Connected to the mongodb')
   }
-);
+)
 
-// Init Appa
-const app = express();
+// init Appa
+const app = express()
 
 // set static folders
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 // set view engine
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'))
 app.engine(
   'handlebars',
   exphbs({
     layoutsDir: 'views',
     defaultLayout: 'home'
   })
-);
-app.set('view engine', 'handlebars');
-app.enable('view cache');
+)
+app.set('view engine', 'handlebars')
+app.enable('view cache')
 
 // bodyParser
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
     extended: false
   })
-);
+)
 
 // Express Session
 app.use(
@@ -61,42 +59,48 @@ app.use(
       maxAge: 3600000 // one hour expiration
     }
   })
-);
+)
 
 app.use(
   expressValidator({
-    errorFormatter: function (param, msg, value) {
+    errorFormatter: function(param, msg, value) {
       const namespace = param.split('.'),
         root = namespace.shift(),
-        formParam = root;
+        formParam = root
 
       while (namespace.length) {
-        formParam += '[' + namespace.shift() + ']';
+        formParam += '[' + namespace.shift() + ']'
       }
       return {
         param: formParam,
         msg: msg,
         value: value
-      };
+      }
     }
   })
-);
+)
 
-app.use(flash());
-app.use(function (req, res, next) {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.user = req.user || null;
-  next();
-});
+app.use(flash())
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.error_msg = req.flash('error_msg')
+  res.locals.user = req.user || null
+  next()
+})
 
 // routes
-const home = require('./routes/homeRouter');
-app.use('/', home);
+app.use('/', require('./routes/homeRouter'))
+app.use('/', require('./routes/registerRouter'))
+app.use('/', require('./routes/loginRouter'))
+app.use('/', require('./routes/logoutRouter'))
+app.use('/addevent', require('./routes/eventRouter'))
+app.use('/mygarage', require('./routes/mygarageRouter'))
+app.use('/maintenance', require('./routes/maintenanceRouter'))
+app.use('/addvehicle', require('./routes/vehicleRouter'))
 
 // Set Port
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000)
 
-app.listen(app.get('port'), function () {
-  console.log('Server started on port ' + app.get('port'));
-});
+app.listen(app.get('port'), function() {
+  console.log('Server started on port ' + app.get('port'))
+})
