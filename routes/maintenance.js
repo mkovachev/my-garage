2e9d65c3bc19c5bcf5da3192c0da1f0e6d5026e0
mkovachev <mkovachev6@gmail.com>
2020-01-06 15:31:27 +0200

    maintenance view working

--- routes/maintenance.js
+++ routes/maintenance.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const router = express.Router()
 const Event = require('../models/event')
+const User = require('../models/user')
 const authGuard = require('../middleware/authGuard')
 
 router.get('/', authGuard.checkAuthenticated, async (req, res) => {
-    const { id } = req.session.user._id;
+
     try {
-        await Event.find({ "owner": id })
-        res.render('maintenance')
+        email = req.session.passport.user
+        const user = await User.findOne({ email })
+        const events = await Event.find({ 'owner': user.id }).limit(12).exec()
+        res.render('maintenance', {
+            user: user,
+            events: events
+        })
     } catch {
         res.redirect('/')
         req.flash('error', 'Please login first')
